26e86e587f012555ff2fd98391ecc4433d76623e
ohtomu <150762200+ohtomu@users.noreply.github.com>
2024-07-12 22:29:49 -0300

    code upgrade and display lenght of each type of char

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -1,40 +1,74 @@
-let includeEspecial = false;
-let includeNumber = false;
-let includeUpper = false;
-let includeLower = false;
+const ESPECIAL_CHAR = `!@#$%^&*()_+[]{}|;:,.<>?/~\"-=`;
+const NUMBER = `0123456789`;
+const LOWERCASE_CHAR = `abcdefghijklmnopqrstuvwxyz`;
+const UPPERCASE_CHAR = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
+const charMap = new Map();
+
+const ESPECIAL_CHARS_N = document.getElementById("especialCharsN");
+const NUMBER_CHARS_N = document.getElementById("numberCharsN");
+const UPPER_CHARS_N = document.getElementById("upperCharsN");
+const LOWER_CHARS_N = document.getElementById("lowerCharsN");
+
+const ESPECIAL_CHARS = document.getElementById("especialChars");
+const NUMBER_CHARS = document.getElementById("numberChars");
+const UPPER_CHARS = document.getElementById("upperChars");
+const LOWER_CHARS = document.getElementById("lowerChars");
+
+const INCLUDE_ESPECIAL = document.getElementById("includeEspecial");//1
+const INCLUDE_NUMBER = document.getElementById("includeNumber");//2
+const INCLUDE_UPPER = document.getElementById("includeUpper");//3
+const INCLUDE_LOWER = document.getElementById("includeLower");//4 
+
+let includeEspecial = INCLUDE_ESPECIAL.checked;
+let includeNumber = INCLUDE_NUMBER.checked;
+let includeUpper = INCLUDE_UPPER.checked;
+let includeLower = INCLUDE_LOWER.checked;
 let passwordLength;
 let password;
 let generate;
-const especialChars = `!@#$%^&*()_+[]{}|;:,.<>?/~\"-=`;
-const numbers = `0123456789`;
-const lowercaseChars = `abcdefghijklmnopqrstuvwxyz`;
-const uppercaseChars = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
-const charMap = new Map();
 
-document.getElementById("includeEspecial").onchange = function(){
-    includeEspecial = document.getElementById("includeEspecial").checked;//1
+ESPECIAL_CHARS_N.textContent = ESPECIAL_CHARS.value;
+NUMBER_CHARS_N.textContent = NUMBER_CHARS.value;
+UPPER_CHARS_N.textContent = UPPER_CHARS.value;
+LOWER_CHARS_N.textContent = LOWER_CHARS.value;
+
+ESPECIAL_CHARS.onchange = function(){
+    ESPECIAL_CHARS_N.textContent = ESPECIAL_CHARS.value;
 };
-document.getElementById("includeNumber").onchange = function(){
+NUMBER_CHARS.onchange = function(){
+    NUMBER_CHARS_N.textContent = NUMBER_CHARS.value;
+};
+UPPER_CHARS.onchange = function(){
+    UPPER_CHARS_N.textContent = UPPER_CHARS.value;
+};
+LOWER_CHARS.onchange = function(){
+    LOWER_CHARS_N.textContent = LOWER_CHARS.value;
+};
+
+INCLUDE_ESPECIAL.onchange = function(){
+    includeEspecial = document.getElementById("includeEspecial").checked;//1
+    console.log('includeEspecial: '+includeEspecial);
+};
+INCLUDE_NUMBER.onchange = function(){
     includeNumber = document.getElementById("includeNumber").checked;//2
-};;
-document.getElementById("includeUpper").onchange = function(){
+    console.log('includeNumber: '+includeNumber);
+};
+INCLUDE_UPPER.onchange = function(){
     includeUpper = document.getElementById("includeUpper").checked;//3
-};;
-document.getElementById("includeLower").onchange = function(){
+    console.log('includeUpper: '+includeUpper);
+};
+INCLUDE_LOWER.onchange = function(){
     includeLower = document.getElementById("includeLower").checked;//4 
-};;
+    console.log('includeLower: '+includeLower);
+};
 
 document.getElementById("send").onclick = function(){
     console.clear();
-    passwordLength = includeEspecial?Number(document.getElementById("especialChars").value):0;
-    passwordLength += includeNumber?Number(document.getElementById("numberChars").value):0;
-    passwordLength += includeUpper?Number(document.getElementById("upperChars").value):0;
-    passwordLength += includeLower?Number(document.getElementById("lowerChars").value):0;
+    passwordLength = includeEspecial?Number(ESPECIAL_CHARS.value):0;
+    passwordLength += includeNumber?Number(NUMBER_CHARS.value):0;
+    passwordLength += includeUpper?Number(UPPER_CHARS.value):0;
+    passwordLength += includeLower?Number(LOWER_CHARS.value):0;
     password = '';
-    console.log(includeEspecial);
-    console.log(includeNumber);
-    console.log(includeUpper);
-    console.log(includeLower);
     console.log(passwordLength);
     if((includeEspecial)||(includeNumber)||(includeUpper)||(includeLower)){
         generateString();
@@ -76,15 +110,13 @@ function generateNumber(){
             break;
     }
     if(generate)
-    //console.log(`passed: ${generate}`)
     return ''+generate;
 }
 
 function getChar(k){
-    charMap.set('1', especialChars.charAt(Math.round(Math.random()*31)));
-    charMap.set('2', numbers.charAt(Math.round(Math.random()*9)));
-    charMap.set('3', uppercaseChars.charAt(Math.round(Math.random()*25)));
-    charMap.set('4', lowercaseChars.charAt(Math.round(Math.random()*25)));
-    console.log(`k: ${k} / values: `+charMap.get('1')+charMap.get('2')+charMap.get('3')+charMap.get('4'))
+    charMap.set('1', ESPECIAL_CHAR.charAt(Math.round(Math.random()*31)));
+    charMap.set('2', NUMBER.charAt(Math.round(Math.random()*9)));
+    charMap.set('3', LOWERCASE_CHAR.charAt(Math.round(Math.random()*25)));
+    charMap.set('4', UPPERCASE_CHAR.charAt(Math.round(Math.random()*25)));
     return charMap.get(k);
 }
